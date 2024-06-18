463a27b975f8809d34b3e0a602004fcaaa9d1694
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-06-18 11:44:01 -0300

    lint: import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,40 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/order': [
+          'error',
+          {
+            groups: ['external', 'builtin', 'internal', 'parent', 'sibling'],
+            pathGroups: [
+              {
+                pattern: 'react+(|-native)',
+                group: 'external',
+                position: 'before',
+              },
+              {
+                pattern: '@+(routes|screens|components|hooks|theme)',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: './',
+                group: 'internal',
+                position: 'before',
+              },
+            ],
+            pathGroupsExcludedImportTypes: ['react+(|-native)'],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+            'newlines-between': 'always',
+          },
+        ],
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-    <h1>BookWise</h1>
+      <h1>BookWise</h1>
     </>
   )
 }
